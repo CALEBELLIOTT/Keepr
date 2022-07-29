c00a963dcb7350b535180b27973169745dcfff19
Caleb Elliott <caleb123elliott@gmail.com>
2022-07-29 14:07:52 -0600

    ini/ connection

---
+++ Keepr.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const useSockets = false
+export const domain = 'dev-cbpq8j47.us.auth0.com'
+export const clientId = 'fHvefOfTXyBtvuwJcs6QLV8eJgVgdZB4'
+export const audience = 'CalebDev.com'
